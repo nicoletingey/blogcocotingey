cebe7c9b19c25cb64cc7151940f7210ae1e249dd
Nicole Tingey <nicole.tingey1@gmail.com>
2016-12-06 14:37:32 -0700

    Updated css styles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	
+	before_action :set_post, only: [:edit, :update, :show, :destroy]
 
 	def index
 		@posts = Post.all
@@ -20,11 +20,9 @@ class PostsController < ApplicationController
     end
 
 	def edit
-		@post = Post.find(params[:id])
 	end
 
 	def update
-		@post = Post.find(params[:id])
 
 		if @post.update(post_params)
 			redirect_to @post
@@ -34,11 +32,9 @@ class PostsController < ApplicationController
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def destroy
-		@post = Post.find(params[:id])
 		@post.destroy
 		redirect_to posts_path
 	end
@@ -46,5 +42,9 @@ class PostsController < ApplicationController
   	private 
   	def post_params
   		params.require(:post).permit(:title, :date, :description)
+  	end
+
+  	def set_post
+  		@post = Post.find(params[:id])
   	end
 end
