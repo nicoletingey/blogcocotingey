bc3c0e69d9e41f03d3838e8b29d4abefe9bd9aaa
Nicole Tingey <nicole.tingey1@gmail.com>
2016-12-07 14:14:19 -0700

    Current updates and changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 	before_action :set_post, only: [:edit, :update, :show, :destroy]
 
 	def index
-		@posts = Post.all
+		@posts = Post.all.order("updated_at DESC")
 	end
 
 	def new
